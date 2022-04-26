8398f84deab77790c9295081aa5a51e903fc4f7a
wargie <wargie@bk.ru>
2022-04-26 07:34:52 +0200

    оптимизаци решения и поиск ошибки

--- task_030.py
+++ task_030.py
@@ -1,3 +1,6 @@
 from math import pi
+from math import acos
 n = pi
-print(n)
+print(f'Число π - {n}')
+n = round ( 2 * acos( 0.0 ), 3 )
+print(f'Округлённое значение числа π с точностью до трёх знаков после запятой - {n}')
