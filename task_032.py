61ca0d54cd42e5246a6504bb410ea53bcd6010d6
wargie <wargie@bk.ru>
2022-04-27 12:29:35 +0200

    Отладка и оптимизация кода к задаче №32

--- task_032.py
+++ task_032.py
@@ -1,1 +1,15 @@
 from random import randint
+
+def ciąg_liczbowy(rozmiar, m, n):
+    return [randint(m, n) for i in range(rozmiar)]
+
+def unikalne_znaczenie(list):
+    return [i for i in set(list)]
+
+rozmiar = 25
+m = 1
+n = 10
+
+origin = ciąg_liczbowy(rozmiar, m, n)
+print(origin)
+print(unikalne_znaczenie(origin))
