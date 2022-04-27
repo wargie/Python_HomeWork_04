49378fc9972e510c719e412ad26f30548b3d7621
wargie <wargie@bk.ru>
2022-04-27 12:49:17 +0200

    Дополнено решение задачи №32

--- task_032.py
+++ task_032.py
@@ -1,15 +1,16 @@
 from random import randint
 
-def ciąg_liczbowy(rozmiar, m, n):
-    return [randint(m, n) for i in range(rozmiar)]
+def ciąg_liczbowy(rozmiar, a, b):
+    return [randint(a, b) for i in range(rozmiar)]
 
 def unikalne_znaczenie(list):
     return [i for i in set(list)]
+a = 1
+b = 20
+cd = 5
+d = 30
+rozmiar = randint(cd, d)
 
-rozmiar = 25
-m = 1
-n = 10
-
-origin = ciąg_liczbowy(rozmiar, m, n)
-print(origin)
-print(unikalne_znaczenie(origin))
+origin = ciąg_liczbowy(rozmiar, a, b)
+print(f'Задана последовательность чисел: {origin}')
+print(f'Cписок неповторяющихся элементов исходной последовательности{unikalne_znaczenie(origin)}')
